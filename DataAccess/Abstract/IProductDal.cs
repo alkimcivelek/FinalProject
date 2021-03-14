eabc340892ff9257cffd72043ad9657ab0e1d436
Alkım Civelek <alkimcivelek@outlook.com>
2021-03-14 22:55:32 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    /*Entities katmanı referans edildi. 
+    Tüm katmanlar referans edilirse 
+    circular dependency denilen kavram ortaya çıkar.*/
+    public interface IProductDal : IEntityRepository<Product>
+    {
+        List<ProductDetailDto> GetProductDetails();
+    }
+}
+
+//Code Refactoring(Kodun iyileştirilmesi)
